73805854cd9fca8b8c392be151870e657b12878d
TusharMaithani-2002 <202051194@iiitvadodara.ac.in>
2023-10-01 12:14:28 +0530

    initial commit

---
+++ components/SearchBar.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+const SearchBar = () => {
+  const handleSubmit = () => {};
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+
+        <input type="text"
+        placeholder="enter product link"
+        className="searchbar-input"
+        />
+
+        <button type="submit" className="searchbar-btn">search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
