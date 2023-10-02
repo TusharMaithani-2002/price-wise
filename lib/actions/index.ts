4061ebe4968c1981fa12d6f0b58955e9273a542a
TusharMaithani-2002 <202051194@iiitvadodara.ac.in>
2023-10-02 16:11:35 +0530

    created search bar

---
+++ lib/actions/index.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { scrapeAmazonProduct } from "../scrapper";
+
+export async function scrapeAndStoreProduct(productUrl : string) {
+
+    if(!productUrl) return;
+
+    try {
+        const scrappedProduct = await scrapeAmazonProduct(productUrl);
+    } catch(error:any) {
+        throw new Error(`Failed to create/update product ${error.message}`);
+    }
+}
