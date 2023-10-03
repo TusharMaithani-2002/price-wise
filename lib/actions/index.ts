a6f53f1f8abae4593fb638f1ec34b9fe5049b9f5
TusharMaithani-2002 <202051194@iiitvadodara.ac.in>
2023-10-03 14:56:17 +0530

    added getProductById action

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -47,3 +47,18 @@ export async function scrapeAndStoreProduct(productUrl : string) {
         throw new Error(`Failed to create/update product ${error.message}`);
     }
 }
+
+export async function getProductById(productId:string) {
+
+    try {
+        connectToDb();
+
+        const product = await Product.findOne({_id:productId});
+        if(!product) return null;
+
+        return product;
+
+    } catch(error:any) {
+        throw new Error(`Failed to fetch product ${error.message}`);
+    }
+}
